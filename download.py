f6b0e661e7ea751a155870053094c05b62b8aebc
mononitogoswami <mononitog@hotmail.com>
2025-04-26 19:33:46 -0400

    Remove unnecessary files. Refactor

--- download.py
+++ download.py
@@ -9,19 +9,19 @@ import pickle
 
 os.makedirs("data/", exist_ok=True)
 if sys.argv[1] == "physio":
-    url = "https://physionet.org/files/challenge-2012/1.0.0/set-a.tar.gz?download"
-    wget.download(url, out="data")
-    with tarfile.open("data/set-a.tar.gz", "r:gz") as t:
+    url = "https://physionet.org/files/challenge-2012/1.0.0/set-a.tar.gz?download"    
+    os.system(f"cd data && curl -L -o physio.gz {url}")
+    with tarfile.open("data/physio.gz", "r:gz") as t:
         t.extractall(path="data/physio")
+    os.system("rm data/physio.gz")
 
 elif sys.argv[1] == "pm25":
+    # Download the dataset
     url = "https://www.microsoft.com/en-us/research/wp-content/uploads/2016/06/STMVL-Release.zip"
-    urlData = requests.get(url).content
-    filename = "data/STMVL-Release.zip"
-    with open(filename, mode="wb") as f:
-        f.write(urlData)
-    with zipfile.ZipFile(filename) as z:
+    os.system(f"cd data && curl -L -o STMVL-Release.zip {url}")
+    with zipfile.ZipFile("data/STMVL-Release.zip") as z:
         z.extractall("data/pm25")
+    os.system("rm data/STMVL-Release.zip")
         
     def create_normalizer_pm25():
         df = pd.read_csv(
